91614afb5ac7017e2cacfed85a47bc6436b2d60a
Peng-git04 <wang04haipeng@gmail.com>
2026-04-20 16:55:56 +0200

    first commit

---
+++ src/components/Cell.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="cell" :class="{ taken: value }" @click="$emit('click')">
+    {{ value }}
+  </button>
+</template>
+
+<script setup lang="ts">
+import type { Player } from "../types/game";
+
+defineProps<{ value: Player | null }>();
+defineEmits<{ (e: "click"): void }>();
+</script>
+
+<style scoped>
+.cell {
+  width: 100px;
+  height: 100px;
+  font-size: 2.5rem;
+  font-weight: bold;
+  background: #1e1e2e;
+  color: #cdd6f4;
+  border: 2px solid #45475a;
+  cursor: pointer;
+  transition: background 0.2s;
+}
+.cell:hover:not(.taken) {
+  background: #313244;
+}
+.taken {
+  cursor: default;
+}
+</style>
