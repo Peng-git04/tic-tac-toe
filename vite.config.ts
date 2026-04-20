114710bb4ff8e725485f1a49e91dfb562221ddb4
Peng-git04 <wang04haipeng@gmail.com>
2026-04-20 17:07:29 +0200

    fix conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-<<<<<<< HEAD
-  base: "/tic-tac-toe/"
-=======
-  base:"/tic-tac-toe/"
->>>>>>> 79a1303fd1a495f4a2d958a3575a656d7de44fdd
-})
+  base: "/tic-tac-toe/",
+});
